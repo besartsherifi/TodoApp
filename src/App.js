7504c0572ae70f84b877704c71d26efa39578012
besartsherifi <59452085+besartsherifi@users.noreply.github.com>
2023-04-19 19:09:03 +0200

    full changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TodoApp = () => {
+
+const  [newItem,setsNewItem] = useState("");
+const  [items,setItems] =useState([]);
+function addItem() {
+
+if (!newItem) {
+  alert("Enter an item");
+  return;
 }
 
-export default App;
+  const item={
+    id:Math.floor(Math.random()*1000),
+    value:newItem
+  };
+  setItems(oldList =>[...oldList,item]);
+  setsNewItem("");
+  console.log(item)
+}
+
+function deleteItem(id){
+  const newArray = items.filter(item =>item.id !==id)
+  setItems(newArray)
+}
+  return (
+    <div className='App'>
+      <h1 className=' todo'>ToDo List</h1>
+      <input className='add' type='text' placeholder='Add a new task... ' value={newItem}
+      onChange={e => setsNewItem(e.target.value)}/>
+      <button onClick={() => addItem()}>Add</button>
+      <ol className='ol'>
+        {items.map(item =>{
+          return(
+            <li key={item.id}>{item.value}<button onClick={()=>deleteItem(item.id)}>Delete</button></li>
+          )
+        })}
+      </ol>
+    </div>
+  );
+};
+
+export default TodoApp;
